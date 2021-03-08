372bb5e0fd32c954eb0ef88def2e58df75f25225
Caz Gabrielse <cgabrielse@outlook.com>
2021-03-07 23:43:22 -0600

    Added function for pagination
    Dynamically adds/displays pages and handles changing pages.

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ FSJS Project 2 - Data Pagination and Filtering
 
 /*
 For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
+   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pageTotal-and-filtering#instructions
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 const itemsPerPage = 9;
@@ -28,8 +28,8 @@ function showPage(list, page)
    {
       if (i >= startIndex && i < endIndex)
       {
-         let studentInfo = list[i].innerHTML;
-         studentInfo = `<li class="student-item cf">
+         let itemInfo = list[i].innerHTML;
+         itemInfo = `<li class="student-item cf">
                      <div class="student-details">
                         <img class="avatar" src="${list[i].picture.medium}" alt="Profile Picture">
                         <h3>${list[i].name.first} ${list[i].name.last}</h3>
@@ -40,7 +40,7 @@ function showPage(list, page)
                      </div>
                   </li>`;
 
-         ul.insertAdjacentHTML('beforeend', studentInfo);
+         ul.insertAdjacentHTML('beforeend', itemInfo);
       }
    }
 
@@ -48,10 +48,41 @@ function showPage(list, page)
 }
 
 /*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
+This function will create and insert/append the elements needed for the pageTotal buttons
 */
+function addPagination(list)
+{
+   const pageTotal = Math.ceil(list.length / itemsPerPage);
+   const ul = document.querySelector('.link-list');
+   ul.innerHTML = '';
 
+   // Add new list > button elements for each page based on total page count
+   for (let i = 0; i < pageTotal; i++)
+   {
+      let button = `<li>
+                       <button type="button">${i + 1}</button>
+                    </li>`;
+      ul.insertAdjacentHTML('beforeend', button)
+   }
+
+   // Assign the child element of the Li to a new variable to keep track of current page
+   let currentPage = ul.firstElementChild.firstElementChild;
+   currentPage.className = 'active';
+
+   // Event that will display a new page corresponding to the page index
+   ul.addEventListener('click', (e) => {
+      if (e.target.type === 'button')
+      {
+         currentPage.classList.remove('active');
+         currentPage = e.target;
+         currentPage.className = 'active';
+         showPage(data, currentPage.textContent);
+      }
+      // Else do nothing
+   });
+}
 
 
 // Call functions
+showPage(data, 1);
+addPagination(data);
