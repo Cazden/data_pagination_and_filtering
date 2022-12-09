9ad6be9fc642b78badbd5977e761176ceff6df1b
Caz Gabrielse <cgabrielse@outlook.com>
2022-12-09 16:08:28 -0600

    added showPage function

--- js/script.js
+++ js/script.js
@@ -11,13 +11,35 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
+const studentsPerPage = 9; // Number of students displayed on a single page
 
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function showPage(list, page) {
+   const startIndex = (page * studentsPerPage) -studentsPerPage;
+   const endIndex = page * studentsPerPage;
+   const studentList = document.querySelector('.student-list');
+   studentList.innerHTML = '';
 
+   for(let i = 0; i < list.length; i++) {
+      if(i >= startIndex && i < endIndex) {
+         let student = `<li class="student-item cf">
+                           <div class="student-details">
+                              <img class="avatar" src="${data[i].picture.large}" alt="Profile Picture">
+                              <h3>${data[i].name.first} ${data[i].name.last}</h3>
+                              <span class="email">${data[i].email}</span>
+                           </div>
+                           <div class="joined-details">
+                              <span class="date">${data[i].registered.date}</span>
+                           </div>
+                        </li>`;
+         studentList.insertAdjacentHTML('beforeend', student); 
+      }
+   }
+}
 
 
 /*
